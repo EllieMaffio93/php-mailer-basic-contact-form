2b8ec119ba410c8252b60488221c90a96a7e7f86
EllieMaffio93 <eleonora.maffioletti93@gmail.com>
2021-01-15 15:31:43 +0100

    added new simplified contact form with super basic style on index.php

--- src/index.php
+++ src/index.php
@@ -6,9 +6,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>A basic php mailer contact form</title>
-    <script type="text/javascript" src="form-validation.js"></script>
 
 </head>
+<!--super basic style-->
+<style>
+
+*{
+    box-sizing:border-box;
+}
+
+
+.form-control{
+    margin-bottom:10px;
+    padding-bottom:20px; /*leaving the space for the alert message*/
+    position:relative;
+
+}
+
+.form-control label{
+    display:inline-block;
+    margin-bottom:10px;
+}
+
+.form-control input{
+    border:2px solid #f0f0f0;
+    border-radius:4px;
+    display:block;
+    padding:10px;
+    font-size:14px;
+}
+
+.form-control small {
+    position:absolute;
+    bottom:0;
+    left:0;
+    visibility:hidden;
+}
+
+.form-control.error small {
+    
+    visibility:visible;
+    color:#e74c3c;
+}
+
+.form-btn{
+    margin-top:20px;
+}
+
+.form-control.success input {
+    border-color:#2ecc71;
+}
+
+.form-control.error input {
+    border-color:#e74c3c;
+}
+
+
+
+</style>
 
 <body>
 
@@ -17,42 +72,34 @@
 
 <!--contact form-->
 
-<form  onsubmit="return validateForm()" method="post" id="mc-embedded-subscribe-2-form" name="mc-embedded-subscribe-2-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-<div class="indicates-required"><span class="asterisk">*</span> Campi richiesti</div>
-<div class="mc-field-group">
-    
-    <input type="text" value="" name="NAME" class="required" id="mc-embedded-subscribe-2-form-NAME" placeholder="Nome *">
-    <span id="mc-embedded-subscribe-2-form-NAME_errortext" class="errortext">Nome obbligatorio</span>
-</div>
-<div class="mc-field-group">
-    
-    <input type="text" value="" name="SURNAME" class="required" id="mc-embedded-subscribe-2-form-SURNAME" placeholder="Cognome *">
-    <span id="mc-embedded-subscribe-2-form-SURNAME_errortext" class="errortext">Nome obbligatorio</span>
-</div>
-<div class="mc-field-group">
-    
-    <input type="email" value="" name="EMAIL" class="required email" id="mc-embedded-subscribe-2-form-EMAIL" placeholder="Email *">
-    <span id="mc-embedded-subscribe-2-form-EMAIL_errortext" class="errortext">Email obbligatoria</span>
-</div>
-<div class="mc-field-group">
-    
-    <input type="text" name="MESSAGE" class="" value="" id="mce-MESSAGE" placeholder="Messaggio">
-</div>
-    <div id="mce-responses" class="clear">
-        <div class="response" id="mce-error-response" style="display:none"></div>
-        <div class="response" id="mce-success-response" style="display:none"></div>
-    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5c771fd39270923ece4828d46_ac778e8164" tabindex="-1" value=""></div>
-    <div class="clear"><input class="form-btn" type="submit" value="SEND" name="subscribe" id="mc-embedded-subscribe-2" class="button"></div>
+<form onsubmit="return checkInputs()" class="form" id="my-form" method="post" class="validate" taget="_blank" novalidate>
+    <div class="form-control">
+        <label>Name*</label>
+        <input type="text" name="NAME" class="required" id="NAME" placeholder="Name*">
+        <small>Error message</small>
     </div>
+    <div class="form-control">
+        <label>Surname*</label>
+        <input type="text" name="SURNAME" class="required" id="SURNAME" placeholder="Surname*">
+        <small>Error message</small>
+    </div>
+    <div class="form-control">
+        <label>E-mail*</label>
+        <input type="email" name="EMAIL" class="required" id="EMAIL" placeholder="Email*">
+        <small>Error message</small>
+    </div>
+    <div class="form-control">
+        <label>Messaggio</label>
+        <input type="textarea" name="MESSAGE" id="MESSAGE" placeholder="Message">
+        <small>Error message</small>
+    </div>
+    <input class="form-btn" type="submit" value="SEND" name="subscribe" id="form-button" class="button">
 </form>
-
-</div>
 
     
 </body>
 
+<script type="text/javascript" src="form-validation.js"></script>
 
 
 
